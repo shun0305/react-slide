076c843136c2d5605626c5cf5451a247f29b84af
shun0305 <shunbbra@icloud.com>
2021-01-26 17:56:12 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SlideRoutes from 'react-slide-routes';
+import { Route, NavLink, useLocation } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const Home = () => <div style={styles.home}>Home</div>;
+const About = () => <div style={styles.about}>About</div>;
+const Contact = () => <div style={styles.contact}>Contact</div>;
+
+const styles = {
+  home: {
+    color: '#fff',
+    fontSize: '64px',
+    background: 'rgba(255, 100, 0)',
+  },
+  about: {
+    color: '#fff',
+    fontSize: '64px',
+    background: 'rgba(0, 150, 255)',
+  },
+  contact: {
+    color: '#fff',
+    fontSize: '64px',
+    background: 'rgba(44, 219, 88)',
+  },
 }
+
+const App = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      <nav>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/contact">Contact</NavLink>
+      </nav>
+
+      <SlideRoutes location={location} duration={500}>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+      </SlideRoutes>
+    </>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import './styles.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(
+
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   rootElement
+// );
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
